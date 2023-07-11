029a7530ff6d14a75df5a2a7bed9cf83de5e6ba0
Arda Kurt <to.arda.kurt@gmail.com>
2023-07-11 09:35:53 +0100

    PostCode Info

--- busBoard.js
+++ busBoard.js
@@ -1,36 +1,77 @@
-import fetch from 'node-fetch'
 
-const stopPointURI = `https://api.tfl.gov.uk/StopPoint/`
+// let stopDetails;
+
+// const { error } = require("winston");
+
+// const { error } = require("winston");
+
+
+// const getArrivals = async (stopPoint) => {
+//     try {
+//         await fetch(`${stopPointURI}${stopPoint}/Arrivals`)
+//             .then(response => response.json())
+//             .then(body => {
+//                 stopDetails = body;
+//                 // console.log(stopDetail);
+//                 arrivalPredictions(stopDetails);
+//             })
+
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+
+// const arrivalPredictions = stopDetails => {
+//     stopDetails.forEach((detail, i) => {
+//         console.log(`${i + 1} : ${detail.platformName} - ${detail.stationName} => ${detail.lineName} towards ${detail.towards} in ${(detail.timeToStation / 60).toFixed(0)} \n`)
+//     });
+// }
+
+// getArrivals(stopPoint);
+
+//Api Variables
+const TFL_URL = `https://api.tfl.gov.uk/StopPoint/`
+const POSTCODE_URL = `https://api.postcodes.io/postcodes`
+
 const stopPoint = `490008660N`
-const POSTCODE_API = `https://api.postcodes.io/postcodes/`
+
+//Form Variables
+const postcodeForm = document.getElementById('postcodeForm')
+const postcode = document.getElementById('postcode');
+const postcodeSubmit = document.getElementById('postcodeSubmit');
 
 
-let stopDetails;
+// Event Listeners
+postcodeForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(postcode.value)
+    busBoard();
+  });
+  
+// Get Postcode Details
 
-
-const getArrivals = async (stopPoint) => {
+const getPostcodeInfo = async (postcode) => {
+    let postCodeInfo;
     try {
-        await fetch(`${stopPointURI}${stopPoint}/Arrivals`)
-            .then(response => response.json())
-            .then(body => {
-                stopDetails = body;
-                // console.log(stopDetail);
-                arrivalPredictions(stopDetails);
+        console.log("loading")
+        const response = await fetch(`${POSTCODE_URL}/${postcode}`)
+            .then(response => {
+                if(response.ok){
+                    postCodeInfo = response.json()
+                } else {
+                    throw new error('Something gone wrong');
+                }
             })
-
-    } catch (error) {
-        console.log(error)
+                
+    } catch (err) {
+        console.log('There was an error', err);
     }
+
+    return postCodeInfo;
 }
 
-
-const arrivalPredictions = stopDetails => {
-    stopDetails.forEach((detail, i) => {
-        console.log(`${i + 1} : ${detail.platformName} - ${detail.stationName} => ${detail.lineName} towards ${detail.towards} in ${(detail.timeToStation / 60).toFixed(0)} \n`)
-    });
+const busBoard = async () => {
+    const postCodeInfo = await getPostcodeInfo(postcode.value);
+    console.log(postCodeInfo)
 }
-
-getArrivals(stopPoint);
-
-
-
