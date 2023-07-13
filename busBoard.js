74ee2dfd07fdda8e482f30a54bd73183d6ec920c
Arda Kurt <to.arda.kurt@gmail.com>
2023-07-13 09:46:25 +0100

    Get Stop Points by Lon Lat Pt1

--- busBoard.js
+++ busBoard.js
@@ -38,8 +38,15 @@ const stopPoint = `490008660N`
 
 //Form Variables
 const postcodeForm = document.getElementById('postcodeForm')
-const postcode = document.getElementById('postcode');
+const postcode = document.getElementById('postcode').value;
 const postcodeSubmit = document.getElementById('postcodeSubmit');
+
+const OPTIONS = [
+    {stopTypes : ""}
+    {radius: 200},
+    {mode_names: true}
+]
+
 
 
 // Event Listeners
@@ -54,24 +61,38 @@ postcodeForm.addEventListener("submit", (e) => {
 const getPostcodeInfo = async (postcode) => {
     let postCodeInfo;
     try {
-        console.log("loading")
+        console.log("loading [getPostcodeInfo]")
         const response = await fetch(`${POSTCODE_URL}/${postcode}`)
-            .then(response => {
-                if(response.ok){
-                    postCodeInfo = response.json()
-                } else {
-                    throw new error('Something gone wrong');
-                }
-            })
+        const body = await response.json();
+        postCodeInfo = await body.result;     
                 
     } catch (err) {
         console.log('There was an error', err);
     }
 
-    return postCodeInfo;
+
+    return {lon : postCodeInfo.longitude, lat : postCodeInfo.latitude };
 }
 
+const getBusStopPointsbyLonLat =  (async (lon, lat) => {
+
+    try {
+        console.log("loading [getBusStopPointsbyLonLat]")
+        // ttps://api.tfl.gov.uk/StopPoint/?lat={lat}&lon={lon}&stopTypes={stopTypes}[&radius][&useStopPointHierarchy][&modes][&categories][&returnLines]
+        const response = await fetch(`${TFL_URL}/?lat=${lat}&lon=${lon}&stopTypes=${OPTIONS.stopTypes}&radius=${OPTIONS.radius}`)
+        const body = await response.json();
+        postCodeInfo = await body.result;     
+                
+    } catch (err) {
+        console.log('There was an error', err);
+    }
+
+
+
+
+})
+
 const busBoard = async () => {
-    const postCodeInfo = await getPostcodeInfo(postcode.value);
-    console.log(postCodeInfo)
+    const postCodeInfo = await getPostcodeInfo(postcode);
+    const bustArrivals = await getBusStopPointsbyLonLat(postCodeInfo.lon, postCodeInfo.lat);
 }
